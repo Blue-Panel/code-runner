96d46329c49e5d3037388b4ea1bb70aa53768b7a
Mihai <mihaighiga14@icloud.com>
2025-03-25 21:17:07 +0000

    moved nav to partials header.php file

--- codeRunner/app/Views/dashboard.php
+++ codeRunner/app/Views/dashboard.php
@@ -1,28 +1,6 @@
 <?php include('partials/header.php') ?>
-<!-- Navigation Bar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <i class="fas fa-robot me-2 text-primary"></i><strong>Code Runner</strong>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <div class="navbar-nav">
-                <div class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
-                        <i class="fas fa-user-circle me-1"></i> Admin User
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <!-- <li><a class="dropdown-item" href="#"><i class="fas fa-user-cog me-1"></i> Profile</a></li> -->
-                        <li><a class="dropdown-item" href="<?php echo base_url() ?>logout"><i class="fas fa-sign-out-alt me-1"></i> Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</nav>
+
+
 
 <div class="container py-4">
     <!-- Status Overview -->
